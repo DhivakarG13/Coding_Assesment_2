9325429e146224221e529d8d49d8d1feffddedfc
Dhivakar Gopi <dhivakar.gopi@solitontech.com>
2025-02-26 20:24:02 +0530

    Removed unwanted files

--- BoilerManager/obj/Release/net8.0/BoilerManager.AssemblyInfo.cs
+++ BoilerManager/obj/Release/net8.0/BoilerManager.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoilerManager")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0acd35df9865fd630aafca6bceef1e507f326fe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoilerManager")]
